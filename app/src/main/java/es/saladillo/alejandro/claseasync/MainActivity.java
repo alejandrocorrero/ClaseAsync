a799e5768d936f8ec3cafcfce3d47a4da849db2e
alex <alejandro.correro@hotmail.com>
2018-01-14 20:55:36 +0100

    cosos

--- app/src/main/java/es/saladillo/alejandro/claseasync/MainActivity.java
+++ app/src/main/java/es/saladillo/alejandro/claseasync/MainActivity.java
@@ -1,22 +1,22 @@
 package es.saladillo.alejandro.claseasync;
 
 import android.os.AsyncTask;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
     private static final Integer NUM_TAREAS = 10;
     @BindView(R.id.prbBar)
     ProgressBar progressBar;
-    @BindView(R.id.prbCircular)
+    @BindView(R.id.btnStart)
     Button btnStart;
     @BindView(R.id.lblMessage)
     TextView lblMessage;
@@ -31,25 +31,68 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-    public void ejecutartarea(){
+
+    @OnClick(R.id.btnStart)
+    public void ejecutartarea() {
         TareaSecundaria tarea = new TareaSecundaria();
         tarea.execute(NUM_TAREAS);
     }
 
-    private class TareaSecundaria extends AsyncTask<Integer,Integer,String>{
+    private class TareaSecundaria extends AsyncTask<Integer, Integer, Integer> {
+
         @Override
-        protected String doInBackground(Integer... integers) {
-            return null;
+        protected Integer doInBackground(Integer... params) {
+            int numTrabajos = params[0];
+            // Se realizan los pasos.
+            for (int i = 0; i < numTrabajos && !isCancelled(); i++) {
+                // Se pone a ejecutarTarea.
+                ejecutarTarea();
+                // Informa del progreso.
+                publishProgress(i + 1);
+            }
+            // Se retorna el número de trabajos realizados.
+            return numTrabajos;
         }
 
         @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
+        protected void onPreExecute() {
+            actualizarBar(true);
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+            actualizarBar(false);
         }
 
         @Override
         protected void onProgressUpdate(Integer... values) {
-            super.onProgressUpdate(values);
+            actualizarProgreso(values[0]);
+        }
+
+        private void ejecutarTarea() {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
+
+    private void actualizarBar(boolean iniciada) {
+        if (!iniciada) {
+            progressBar.setProgress(0);
+            lblMessage.setText("");
+        }
+        btnStart.setEnabled(!iniciada);
+        btnStart.setEnabled(iniciada);
+        progressBar.setVisibility(iniciada ? View.VISIBLE : View.INVISIBLE);
+        lblMessage.setVisibility(iniciada ? View.VISIBLE : View.INVISIBLE);
+        prbCircular.setVisibility(iniciada ? View.VISIBLE : View.INVISIBLE);
+    }
+
+    public void actualizarProgreso(int values) {
+        progressBar.setProgress(values);
+        lblMessage.setText("Trabajos realizados: " + values + " de " + NUM_TAREAS);
+    }
+
 }
